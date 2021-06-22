41a3f90a56de2f1a0fad6a925dfce5d8ee8d6b98
Rzky3016 <83935714+Rzky3016@users.noreply.github.com>
2021-06-22 12:36:10 +0700

    Update song.py

--- MusicMan/modules/song.py
+++ MusicMan/modules/song.py
@@ -65,7 +65,7 @@ def song(client, message):
         results[0]["views"]
 
     except Exception as e:
-        m.edit("❌ Lagu Tidak ditemukan.\n\nCoba Masukan Judul lagu yang lebih jelas.")
+        m.edit("❌ Lagu Tidak ditemukan.\n\nCoba Masukan Judul lagu secara detail.")
         print(str(e))
         return
     m.edit("⬇️ **Sedang Mendownload Lagu**")
@@ -74,7 +74,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by @Lunatic0de**"
+        rep = "**🎵 Uploaded by @Nopegoodloking**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
